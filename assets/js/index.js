ffd3b80e96e54904bff9b193641625209fa09ca3
KhatriShashi <shashikrkhatri9546@gmail.com>
2023-11-06 23:37:43 +0530

    css add

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,26 @@
+let menu = document.querySelector(".menu");
 document.querySelector(".menu-icon").addEventListener('click', () => {
-    let menu = document.querySelector(".menu");
     menu.classList.toggle("menu-open");
 })
 
 window.addEventListener("scroll", (e) => {
-    if (window.scrollY <5) {
-        document.querySelector(".navbar-section").style.background= "none";
+
+    if (window.scrollY < 5) {
+        document.querySelector(".navbar-section").style.background = "none";
     } else {
-        document.querySelector(".navbar-section").style.background= "#faf6f0";
+        document.querySelector(".navbar-section").style.background = "#faf6f0";
     }
 })
 
-// active menu
-document.querySelectorAll(".menu ul li a").forEach((item)=>{
-    item.addEventListener("click",()=>{
-        document.querySelectorAll(".menu ul li a").forEach((item)=>{
-            item.classList.remove("active");
-        })
-        item.classList.add("active");
-    })
-});
+
+
+let section = document.querySelectorAll('section')
+let lists = document.querySelectorAll('.menu ul li a');
+function activeLink() {
+    lists.forEach((item) =>
+        item.classList.remove('active'));
+        this.classList.add('active');
+        menu.classList.remove("menu-open");
+}
+lists.forEach((item) =>
+    item.addEventListener('click', activeLink));
